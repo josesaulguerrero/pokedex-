3409f8bca6f67ac33daf511956dffa2ceaef9daa
Jose Saul Serrano <josesaulserrano15@gmail.com>
2022-01-06 10:11:58 -0500

    created the basic action interfaces for the home page

--- src/state/actions/home.actions.ts
+++ src/state/actions/home.actions.ts
@@ -1,3 +1,4 @@
+import { PokemonBasicDetails } from "../../globalTypes";
 import { Home } from "../actionTypes";
 
 export interface FetchPokemonsRequest {
@@ -5,4 +6,15 @@ export interface FetchPokemonsRequest {
 	payload: {
 		url: string;
 	};
+}
+
+export interface FetchPokemonSuccess {
+	type: Home.Types.FETCH_POKEMONS_SUCCESS;
+	payload: {
+		pokemons: PokemonBasicDetails[];
+	};
+}
+
+export interface FetchPokemonsFailed {
+	type: Home.Types.FETCH_POKEMONS_FAILED;
 }
