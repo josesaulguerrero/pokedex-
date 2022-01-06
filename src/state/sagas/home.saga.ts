ced2a564b0da26ff6b54c896918496d2afeadc1b
Jose Saul Serrano <josesaulserrano15@gmail.com>
2022-01-06 17:00:24 -0500

    created the home saga and now it retrieves the normalized data!

--- src/state/sagas/home.saga.ts
+++ src/state/sagas/home.saga.ts
@@ -1,16 +1,41 @@
 // libraries
 import { call, put, takeEvery } from "redux-saga/effects";
+import axios, { AxiosResponse } from "axios";
 // action data type
-// import { Action } from "../actions";
 import { FetchPokemonsRequest } from "../actions/home.actions";
 // action types
 import { Home } from "../actionTypes";
-import axios, { AxiosResponse } from "axios";
+// global types
+import { HomeBasicAPIResponse, PokemonBasicDetails } from "../../globalTypes";
+
+const getNormalizedData = ({ data }: AxiosResponse) => {
+	let promises = data.results.map((item: HomeBasicAPIResponse) => {
+		return axios
+			.get(item.url)
+			.then((response) => response.data)
+			.then(
+				(response): PokemonBasicDetails => ({
+					name: response.name,
+					pokedexId: response.id,
+					spriteUrl: response.sprites.front_default,
+					types: response.types,
+				})
+			)
+			.catch((error) => {
+				throw new Error(error);
+			});
+	});
+	return Promise.all(promises);
+};
 
 function* fetchPokemons(action: FetchPokemonsRequest) {
 	try {
 		const data: AxiosResponse = yield call(axios.get, action.payload.url);
-		yield console.log(data.data);
+		let normalizedData: PokemonBasicDetails[] = yield getNormalizedData(data);
+		yield put({
+			type: Home.Types.FETCH_POKEMONS_SUCCESS,
+			payload: { pokemons: normalizedData },
+		});
 	} catch (error) {
 		yield put({ type: Home.Types.FETCH_POKEMONS_FAILED });
 	}
