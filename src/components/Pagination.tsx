ed18537845104073c27d6f206813cc75d4421e56
Jose Saul Serrano <josesaulserrano15@gmail.com>
2022-01-06 14:36:52 -0500

    created the pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,68 @@
+// components
+import { FC, useState } from "react";
+
+export const Pagination: FC = () => {
+	const pagesAmount = Math.ceil(1118 / 20); // number of pokemons in the API divided by pokemons per page
+	let allPages = []; // list with page's id's
+	for (let i = 1; i <= pagesAmount; i++) {
+		allPages.push(i);
+	}
+	const [lowerLimit, setLowerLimit] = useState(0);
+	const [upperLimit, setUpperLimit] = useState(4); //i will have 4 buttons, so the upper limit is 4.
+	const [currentPage, setCurrenPage] = useState(1);
+	const currentPages = allPages.slice(lowerLimit, upperLimit);
+
+	const goToPreviousPage = () => {
+		setCurrenPage((currentPage) => {
+			const newCurrentPage = currentPage - 1;
+			if (newCurrentPage < lowerLimit) {
+				setLowerLimit((currentLimit) => currentLimit - 4);
+				setUpperLimit((currentLimit) => currentLimit - 4);
+			}
+			return newCurrentPage;
+		});
+	};
+
+	const goToNextPage = () => {
+		setCurrenPage((currentPage) => {
+			const newCurrentPage = currentPage + 1;
+			if (newCurrentPage > upperLimit) {
+				setLowerLimit((currentLimit) => currentLimit + 4);
+				setUpperLimit((currentLimit) => currentLimit + 4);
+			}
+			return newCurrentPage;
+		});
+	};
+
+	const renderPaginationButtons = (pageNumber: number): JSX.Element => (
+		<li
+			key={pageNumber}
+			className={`paginationButton ${
+				pageNumber === currentPage && "current"
+			}`}
+			onClick={() => setCurrenPage(pageNumber)}
+		>
+			{pageNumber}
+		</li>
+	);
+
+	return (
+		<section className="Pagination">
+			<button
+				onClick={goToPreviousPage}
+				className="previousPage"
+				disabled={currentPage === 1}
+			>
+				Previous
+			</button>
+			<ul>{currentPages.map(renderPaginationButtons)}</ul>
+			<button
+				onClick={goToNextPage}
+				className="nextPage"
+				disabled={currentPage === pagesAmount}
+			>
+				Next
+			</button>
+		</section>
+	);
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+// libraries
+import { FC } from "react";
+import { Pagination } from "../components/Pagination";
+
+export const Home: FC = () => {
+	return (
+		<section className="Home">
+			<Pagination />
+		</section>
+	);
+};
